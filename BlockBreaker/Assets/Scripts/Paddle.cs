b0d12914e12c083e4e6e5d998bb8b72b876c7471
BrianWhitePerth <1020070@student.sae.edu.au>
2020-09-29 13:50:46 +0800

    Adding Block Breaker Project

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UIElements;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
+
+        this.transform.position = paddlePos;
+    }
+}
